1d76a1932b9ab45980f225e48b9eb79e9d619ef3
Dann Lara <dkubdannspc@gmail.com>
2026-02-17 08:55:47 -0600

    Update environment variables for testing

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 export const ENV = {
-    WHATSAPP_NUMBER: "524623798844", // Formato: CódigoPaís + Número
-    GOOGLE_MAPS_LINK: "https://www.google.com/maps/place/DetaileMX/@20.6990956,-101.3490686,17z/data=!3m1!4b1!4m6!3m5!1s0x842c7f3c7e2922d7:0x1f682aa530981be8!8m2!3d20.6990956!4d-101.3490686!16s%2Fg%2F11lg3xz946?entry=ttu&g_ep=EgoyMDI2MDIwMS4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
-    EMAIL_CONTACT: "hola@detaiemx.com",
-    INSTAGRAM_USER: "detailemx",
-    TIKTOK_USER: "detailemx",
-    FACEBOOK_USER: "detailemx"
+    WHATSAPP_NUMBER: "x",
+    GOOGLE_MAPS_LINK: "https://maps.app.goo.gl/y4Btipt5H7DmHt1H9",
+    EMAIL_CONTACT: "Test@test.com",
+    INSTAGRAM_USER: "test",
+    TIKTOK_USER: "test",
+    FACEBOOK_USER: "test"
 };
